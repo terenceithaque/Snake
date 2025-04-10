e6d01f17d286285c3a8aaf628bbeae8b306b339f
terenceithaque <96685979+terenceithaque@users.noreply.github.com>
2025-04-10 15:37:19 +0200

    Implémenté directions up, right, left, down

--- snake-vTerence.py
+++ snake-vTerence.py
@@ -20,28 +20,51 @@ can = Canvas(tk, width=500, height=500, bg='black')
 
 # Calcule la nouvelle frame du jeu
 def computeNextFrame(numFrame, coordonnee):
+    global direction
     # Affiche le numéro de la frame
-    print(numFrame)
+    print(numFrame, direction)
     numFrame = numFrame + 1
     
     # Effacer le canevas
     can.delete("all")
     
+    x = coordonnee[0]
+    y = coordonnee[1]
+    
     # Mise à jour des coordonnées
-    coordonnee += -20
+    if direction == "up":
+        y = y - 20
+        print("y:", y)
+        
+    elif direction == "down":
+        y = y + 20
+        print("y:", y)
+        
+    elif direction == "right":
+        x = x + 20
+        print("x:", x)
+        
+    elif direction == "left":
+        x = x - 20
+        print("x:", x)
     
-    if coordonnee < 0:
-        coordonnee = 0
+    if x < 0:
+        x = 500
     
-    elif coordonnee > 500:
-        coordonnee = 0
-    
+    elif x > 500:
+        x = 0
+        
+    if y < 0:
+        y = 500
+        
+    elif y > 500:
+        y = 0
     # Dessin de la tête du serpent
-    can.create_rectangle(coordonnee, 200, coordonnee + 20, 220, outline = "yellow", fill="red")
+    can.create_rectangle(x, y, x + 20, y+20, outline = "yellow", fill="red")
     
     
     # Calcule une nouvelle frame dans les 100 ms
-    tk.after(100, lambda:computeNextFrame(numFrame, coordonnee))
+    tk.after(100, lambda:computeNextFrame(numFrame, [x, y]))
 
 
 
@@ -55,16 +78,41 @@ def right(event):
     global direction
     direction = "right"
     print(direction)
+    
+    
+def left(event):
+    global direction
+    direction = "left"
+    print(direction)
+    
+def down(event):
+    global direction
+    direction = "down"
+    print(direction)
+
+def up(event):
+    global direction
+    direction = "up"
+    print(direction)
 
 
 direction = "up"
 
 
 # Construction de la première étape de simulation
-computeNextFrame(0, 500)
+computeNextFrame(0, [0, 0])
 
 # Appuyer sur la touche "d" appellera la fonction right()
 tk.bind("d", right)
+
+# Appuyer sur la touche "q" appellera la fonction left()
+tk.bind("q", left)
+
+# Appuyer sur la touche "s" appellera la fonction down()
+tk.bind("s", down)
+
+# Appuyer sur la touche "z" appellera la fonction up()
+tk.bind("z", up)
 
 # lancement de la boucle principale qui écoute les évènements (claviers...)
 tk.mainloop() # Cet appel doit être la derniere instruction du programme
