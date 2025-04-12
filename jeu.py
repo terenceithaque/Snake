a8c6128a1ae6571ced747afc5301a459d0a2f58e
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-04-12 11:21:33 +0200

    Demande au joueur s'il souhaite rejouer après chaque partie terminée

--- jeu.py
+++ jeu.py
@@ -1,4 +1,34 @@
 "jeu.py contient la classe Jeu qui représente une partie."
-# Importer et initialiser pygame
+# Importations nécessaires
 import pygame
 pygame.init()
+
+
+class Jeu:
+    "Représente une partie en cours avec sa propre fenêtre et éléments."
+    def __init__(self, hauteur_fenetre = 800, largeur_fenetre = 600):
+        """
+        Initialise la partie.
+        - hauteur_fenetre: hauteur de la fenêtre en pixels, par défaut 800px
+        - largeur_fenetre: largeur de la fenêtre en pixels, par défaut 600px."""
+
+
+        # Initialiser la fenêtre de jeu
+        self.fenetre = pygame.display.set_mode((hauteur_fenetre, largeur_fenetre))
+        pygame.display.set_caption("Python Snake !")
+
+
+    def executer(self):
+        "Exécute la boucle de jeu."
+        # Mettre la variable d'exécution à True
+        execution = True
+
+        # Commencer la boucle de jeu
+        while execution:
+
+            # Capturer tous les événements utilisateur de la fenêtre (clics de souris, touches pressées, etc.)
+            for evenement in pygame.event.get():
+                # Si le joueur souhaite quitter le jeu
+                if evenement.type == pygame.QUIT:
+                    # Arrêter la boucle de jeu$
+                    execution = False    
--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-"main.py lance le jeu"
+"main.py lance le jeu."
+# Importations nécessaires
 import pygame
 pygame.init()
+from tkinter import messagebox
+from jeu import *
+
+# Instancier et exécuter une première partie, avec une fenêtre de dimebnsions 800x600
+jeu = Jeu(800, 600)
+jeu.executer()
+# Quitter pygame quand la partie est finie.
+pygame.quit()
+
+
+# Entrer dans une boucle pour demander au joueur s'il souhaite rejouer
+while True:
+    rejouer = messagebox.askyesno("Rejouer ?", "Souhaitez-vous rejouer ?")
+    # Si le joueur veut rejouer
+    if rejouer:
+        # Réinitialiser pygame et relancer le jeu
+        pygame.init()
+        jeu = Jeu(800, 600)
+        jeu.executer()
+        # Quand la partie est finie, quitter pygame
+        pygame.quit()
+
+
+    # Si le joueur ne souhaite pas rejouer
+    else:
+        break    
+
+
+
 
