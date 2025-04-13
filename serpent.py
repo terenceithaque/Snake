b115949a66793f409d4e4b863b20fe87cd95d8d0
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-04-13 10:24:11 +0200

    Ajouté serpent.py

---
+++ serpent.py
@@ -0,0 +1,8 @@
+"serpent.py contient une classe Serpent qui représente le serpent du joueur."
+import pygame
+
+class Serpent(pygame.sprite.Sprite):
+    "Serpent du joueur"
+    def __init__(self):
+        "Constructeur"
+        pass
