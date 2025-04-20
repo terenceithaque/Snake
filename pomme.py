45d0eccfebc3f52b7cce5a96981ed1076cc02632
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-04-20 20:46:19 +0200

    Ajouté la classe Pomme

--- pomme.py
+++ pomme.py
@@ -1,3 +1,36 @@
 "pomme.py contient la classe Pomme, représentant une pomme consommable"
 # Importations nécessaires
 import pygame
+import random
+
+
+class Pomme(pygame.sprite.Sprite):
+    "Une pomme consommable par le joueur"
+    def __init__(self, x1:int, y1:int, x2:int, y2:int, image:str, ecran:pygame.Surface) -> None:
+        """Constructeur de la pomme apparaissant à des coordonnées aléatoires.
+        
+        - x1: Coordonnée x minimale de la pomme dans l'intervalle de tirage
+        - y1: Coordonnée y minimale de la pomme dans l'intervalle de tirage
+        - x2: Coordonnée x maximale de la pomme dans l'intervalle de tirage
+        - y2: Coordonnée y maximale de la pomme dans l'intervalle de tirage
+        - image: chemin du fichier image représentant la pomme
+        - ecran: surface d'affichage de la pomme."""
+
+        # Surface d'affichage de la pomme
+        self.ecran = ecran
+
+
+        # Image de la pomme
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+
+
+        # Tirage au hasard des coordonnées de la pomme
+        self.rect.x = random.randint(x1, x2)
+        self.rect.y = random.randint(y1, y2)
+
+
+    def afficher(self) -> None:
+        "Affiche la pomme à l'écran"
+        self.ecran.blit(self.image, (self.rect.x, self.rect.y))    
